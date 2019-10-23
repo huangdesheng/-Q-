870be955aed1bf91dd19ec66b9ebdaa4958f315e
小明2582 <15815542582@163.com>
2019-10-23 14:10:50 +0800

    2582:修改了冲突

--- config/index.js
+++ config/index.js
@@ -21,11 +21,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-<<<<<<< HEAD
-    host: "192.168.18.127", // can be overwritten by process.env.HOST
-=======
     host: '192.168.18.127', // can be overwritten by process.env.HOST
->>>>>>> hds
     port: 9527, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
