722cee1430ca23a4a77aaf993e32e0dbfc7a809d
huangdesheng <qq1083879945@126.com>
2019-10-28 20:51:51 +0800

    '0102:解决prop的冲突'

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,11 +4,7 @@ module.exports = {
   // BASE_API: '"http://zc.qiaox.net/"'
   BASE_API: '"http://ring.qxiao.net/"'
   // BASE_API: '"http://ax.qxiao.net/"'
-<<<<<<< HEAD
-  BASE_API: '"http://qxiao.natapp1.cc/"'
-=======
   // BASE_API: '"http://qxiao.natapp1.cc/"'
->>>>>>> 311209956fa7a7d1ed3e0afd2b1d474cae7035da
   // BASE_API: '"http://qxiaonmq.natapp1.cc/"'
   // BASE_API: '"https://zc.qxiao.net/"'
 };
