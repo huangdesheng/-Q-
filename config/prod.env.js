6c0430a8aab196773ad0793937493f0a912af68e
小明2582 <15815542582@163.com>
2019-10-29 19:34:28 +0800

    2582:2019.10.29合并冲突

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,13 +2,9 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"http://zc.qiaox.net/"'
-  BASE_API: '"http://ring.qxiao.net/"'
+  // BASE_API: '"http://ring.qxiao.net/"'
   // BASE_API: '"http://ax.qxiao.net/"'
-<<<<<<< HEAD
   BASE_API: '"http://qxiao.natapp1.cc/"'
-=======
-  // BASE_API: '"http://qxiao.natapp1.cc/"'
->>>>>>> 311209956fa7a7d1ed3e0afd2b1d474cae7035da
   // BASE_API: '"http://qxiaonmq.natapp1.cc/"'
   // BASE_API: '"https://zc.qxiao.net/"'
 };
