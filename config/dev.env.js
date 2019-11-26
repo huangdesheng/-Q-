e52e9d716ef0f0be66a1f5d4647a3527a096df72
huangdesheng <qq1083879945@126.com>
2019-11-26 09:07:35 +0800

    hds:提交包

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,8 +5,8 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"http://192.168.18.200:8080/"'
-  // BASE_API: '"http://192.168.18.200:8080/"'
+  BASE_API: '"http://192.168.18.200:8080/"'
   // BASE_API: '"http://qxiao.natapp1.cc/"'
   //BASE_API: '"http://192.168.0.106:8080/"'
-  BASE_API: '"http://qxiaonmq.natapp1.cc/"'
+  // BASE_API: '"http://qxiaonmq.natapp1.cc/"'
 });
